1b74a7782e494d9dbd445f4578c94e784da8ae86
abhishekty17 <abhishekty17@gmail.com>
2022-11-24 16:20:36 +0530

    abhishek

--- frontend/src/components/Class Schedule/Sem_1..js
+++ frontend/src/components/Class Schedule/Sem_1..js
@@ -102,6 +102,7 @@ const Sem_1 = () => {
             id="subject"
             name="subject"
             value={subject}
+            required
             onChange={(e) => setsubject(e.target.value)}>
             <option required>Select Subject</option>
             <option value="Software Design & Programming">
@@ -128,6 +129,7 @@ const Sem_1 = () => {
             id="date"
             aria-describedby="date"
             value={date}
+            required
             onChange={(e) => setdate(e.target.value)}
           />
         </div>
@@ -140,6 +142,7 @@ const Sem_1 = () => {
             className="form-control"
             id="time"
             value={time}
+            required
             onChange={(e) => settime(e.target.value)}
           />
         </div>
