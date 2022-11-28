4f8f6462ebc6d22653167684ec6054b995375554
AnasSaifeee <anas.saifi@iic.ac.in>
2022-11-28 16:22:37 +0530

    cmmmmt

--- frontend/src/components/Assignment_Upload/Upload_assignment_sem4.js
+++ frontend/src/components/Assignment_Upload/Upload_assignment_sem4.js
@@ -1,39 +1,80 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from '../Student_dashboard/Navbar';
 import './upload_assignment.css';
-
+import jwt from 'jsonwebtoken'
+import { useNavigate } from "react-router-dom"
 
 function UploadAssignmentsem4() {
+    const navigate = useNavigate();
     const [file, setFile] = useState("");
     const [subject, setsubject] = useState("");
-    console.log(file);
-    console.log(subject);
+    const [deadline,setdeadline]=useState("")
+    const [teacher,setTeacher] = useState("")
+    const [description,setdescription]=useState("")
+
+
+     async function populate(e) {
+    const req = await fetch('http://localhost:4000/upload', {
+      headers: {
+        'x-access-token': localStorage.getItem('token'), //
+      },
+    })
+    const data = await req.json();
+    setTeacher(data.name)
+  }
+
     async function Upload() {
         const response = await fetch("http://localhost:4000/upload", {
             method: "POST",
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
+                'x-access-token': localStorage.getItem('token'), 
+
             },
             body: JSON.stringify({
-                file
+                file,
+                subject,
+                deadline,
+                semester:"Sem-4",
+                teacher,
+                description
             }),
         });
 
         const data = await response.json();
+        console.log(data);
     }
+
+    //--------------------
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      const user = jwt.decode(token)
+      console.log(user)
+      if (!user) {
+        localStorage.removeItem('token')
+        navigate("/Teacherdashboard");
+      } else {
+        populate()
+
+      }
+    }
+  }, [])
     return (
         <div className='uploadassignmentbody'>
             <Navbar />
             <div className='uploadheading'>
                 <h2>Upload Assignment</h2>
             </div>
-
+          <div className='uploadassignmentcontent'>
+              <div className=" mb-3" >
+                   <label htmlFor="form-label">Deadline</label>
+                   <input type="date" value={deadline} onChange={(e)=>setdeadline(e.target.value)} />
+              </div>
+          </div>
             <div className='uploadassignmentcontent'>
-
-                {/* select subject */}
-
                 <div className=" mb-3">
                     <label className="form-label">Select Subject</label>
                     <select
@@ -60,11 +101,11 @@ function UploadAssignmentsem4() {
                 </div>
 
                 {/* upload file */}
-
+                
                 <div className="file-card">
                     <div className='file-inputs'>
                         <input type="file" value={file} onChange={(e) => setFile(e.target.value)} />
-                        <button onClick={Upload}>Upload</button>
+                        <button >Upload</button>
                     </div>
                     <div className='infocontent'>
                         <p className="main">Supported files</p>
@@ -76,15 +117,15 @@ function UploadAssignmentsem4() {
 
                 <div class="mb-3 mt-4">
                     <label for="exampleFormControlTextarea1" class="form-label">Discription</label>
-                    <textarea class="form-control" placeholder='optional' id="exampleFormControlTextarea1" rows="3"></textarea>
+                    <textarea class="form-control" placeholder='optional' id="exampleFormControlTextarea1" rows="3" value={description} onChange={(e)=>setdescription(e.target.value)} ></textarea>
                 </div>
 
                 {/* submit button */}
 
-                <button className='submitbutton'>Submit</button>
+                <button className='submitbutton' onClick={Upload} >Submit</button>
             </div>
         </div>
     )
 }
 
-export default UploadAssignmentsem4
+export default UploadAssignmentsem4;
