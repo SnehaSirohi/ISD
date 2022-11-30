422130cc3a91d9ec0fe25fc474c98a3687c727f1
AnasSaifeee <AnasSaifeee@anas.saifi@iic.ac.in>
2022-11-30 11:49:15 +0530

    cmmt

--- frontend/src/components/Scheduled_Class_List/classreport_student.js
+++ frontend/src/components/Scheduled_Class_List/classreport_student.js
@@ -11,6 +11,9 @@ var XLSX = require("xlsx");
 const Classreport = () => {
     const navigate = useNavigate();
     const [classes,setClasses]=useState([]);
+    const newdate= new Date()
+    const monthval= newdate.getMonth()+1;
+    const day =newdate.getDate()
     const fetchdata=async()=>{
         const response=await fetch("http://localhost:4000/classschedule", {
             method: "GET",
@@ -20,7 +23,17 @@ const Classreport = () => {
                 'x-access-token': localStorage.getItem('token'), //
             }})
             const json = await response.json()
-                setClasses(json.data)
+            let data = json.data.filter((data)=>{
+              if(data.date.slice(5,7)==monthval && data.date.slice(8,10)>=day)
+              {
+                  return data
+              }
+              else if(data.date.slice(5,7)>monthval)
+              {
+                return data
+              }
+            })
+                setClasses(data)
 
       }
       useEffect(() => {
