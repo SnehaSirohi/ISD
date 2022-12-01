f6c207828a8e0b5aab6869abb9ed6edab4c13793
abhishekty17 <abhishekty17@gmail.com>
2022-12-01 13:55:43 +0530

    commit

--- frontend/src/components/Student_dashboard/Navbar.js
+++ frontend/src/components/Student_dashboard/Navbar.js
@@ -30,35 +30,30 @@ function Navbar() {
         })
 
         const json = await req.json()
-        if(json.status == "ok")
-        {
-            if(json.semester == "Sem-1")
-            {
+        if (json.status == "ok") {
+            if (json.semester == "Sem-1") {
                 setSem1(true)
             }
 
-            else if(json.semester == "Sem-2")
-            {
+            else if (json.semester == "Sem-2") {
                 setSem2(true)
             }
-            else if(json.semester == "Sem-3")
-            {
+            else if (json.semester == "Sem-3") {
                 setSem3(true)
             }
-            
-            else if(json.semester == "Sem-4")
-            {
+
+            else if (json.semester == "Sem-4") {
                 setSem4(true)
             }
         }
         console.log(json);
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         const token = localStorage.getItem('token')
-        if (token){
+        if (token) {
             const user = jwt.decode(token)
-            if(!user){
+            if (!user) {
                 localStorage.removeItem('token')
                 navigate("/");
             } else {
@@ -87,61 +82,61 @@ function Navbar() {
                             <span>Classopedia</span>
                         </li>
                         <li className="nav-text">
-                            <AiIcons.AiFillHome />
+                            <AiIcons.AiFillHome color='rgb(0, 104, 74)' />
                             <span><Link to="/dashboard">Home</Link></span>
                         </li>
                         <li className="nav-text">
-                            <FaIcons.FaUserCircle />
+                            <FaIcons.FaUserCircle color='rgb(0, 104, 74)' />
                             <span><Link to="/dashboard/profile">Profile</Link></span>
                         </li>
                         <li className="nav-text" onClick={showsemesterhandler} style={{
                             background: sem ? "#2BB0A8" : ""
                         }}>
-                            <FaIcons.FaPenSquare />
+                            <FaIcons.FaPenSquare color='rgb(0, 104, 74)' />
                             <span>Subjects</span>
-                            <FaIcons.FaCaretDown />
+                            <FaIcons.FaCaretDown color='rgb(0, 104, 74)' />
                         </li>
                         <li className="semester" style={{
                             display: sem ? "block" : "none"
                         }}>
-                          {sem1 && <>
-                            <Link to="/scheduledclass/sem1"><p>Algorithms and Data Structure</p></Link>
-                            <Link><p>Software Design & Programming</p></Link>
-                            <Link><p>Mathematical Foundation of Computing</p></Link>
-                            <Link><p>Computer System Architecture</p></Link>
+                            {sem1 && <>
+                                <Link to="/scheduledclass/sem1"><p>Algorithms and Data Structure</p></Link>
+                                <Link><p>Software Design & Programming</p></Link>
+                                <Link><p>Mathematical Foundation of Computing</p></Link>
+                                <Link><p>Computer System Architecture</p></Link>
                             </>}
 
                             {sem2 && <>
-                            <Link to="/"><p>Computer Communication and Networks</p></Link>
-                            <Link><p>Operating Systems</p></Link>
-                            <Link><p>Database Systems</p></Link>
-                            <Link><p>Applied Machine Learning</p></Link>
+                                <Link to="/"><p>Computer Communication and Networks</p></Link>
+                                <Link><p>Operating Systems</p></Link>
+                                <Link><p>Database Systems</p></Link>
+                                <Link><p>Applied Machine Learning</p></Link>
                             </>}
 
                             {sem3 && <>
-                            <Link to="/"><p>Information System Design</p></Link>
-                            <Link><p>Cloud Computing</p></Link>
-                            <Link><p>Software Engineering</p></Link>
-                            <Link><p>IT Planning and Management</p></Link>
+                                <Link to="/"><p>Information System Design</p></Link>
+                                <Link><p>Cloud Computing</p></Link>
+                                <Link><p>Software Engineering</p></Link>
+                                <Link><p>IT Planning and Management</p></Link>
                             </>}
 
                             {sem4 && <>
-                            <Link to="/"><p>Internet of Things Systems, Security and Cloud</p></Link>
-                            <Link><p>Health Informatics</p></Link>
-                            <Link><p>Dissertation Project</p></Link>
-                            <Link><p>Research Methods in Informatics</p></Link>
+                                <Link to="/"><p>Internet of Things Systems, Security and Cloud</p></Link>
+                                <Link><p>Health Informatics</p></Link>
+                                <Link><p>Dissertation Project</p></Link>
+                                <Link><p>Research Methods in Informatics</p></Link>
                             </>}
                         </li>
                         <li className="nav-text">
-                            <FaIcons.FaRegSun />
+                            <FaIcons.FaRegSun color='rgb(0, 104, 74)' />
                             <span>Setting</span>
                         </li>
                         <li className="nav-text">
-                            <FaIcons.FaRegQuestionCircle />
+                            <FaIcons.FaRegQuestionCircle color='rgb(0, 104, 74)' />
                             <span>FAQ</span>
                         </li>
                         <li className="nav-text">
-                            <FaIcons.FaSignInAlt />
+                            <FaIcons.FaSignInAlt color='rgb(0, 104, 74)' />
                             <span onClick={() => {
                                 localStorage.removeItem('token')
                                 navigate("/")
