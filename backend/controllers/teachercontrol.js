82a4aa8d0da360cfabf156df515111d89221a923
SnehaSirohi <sneha.2021@iic.ac.in>
2022-12-02 17:11:18 +0530

    jjj

--- backend/controllers/teachercontrol.js
+++ backend/controllers/teachercontrol.js
@@ -445,11 +445,22 @@ const sem4Attendance = async (req, res) => {
 }
 
 const Sem1AttendanceReport = async (req, res) => {
-  return res.status(200).json({
-    success: true,
-    data: await Sem1Attendance.find({}),
-  });
+  const token = req.headers["x-access-token"];
 
+  try {
+    const decoded = jwt.verify(token, "secret1234");
+    const Teacher_id = decoded.Teacher_id;
+    const teacher = await Teacher.findOne({ Teacher_id: Teacher_id });
+    console.log(Teacher_id)
+    return res.status(200).json({
+      success: true,
+      data: await Sem1Attendance.find({}),
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", error: "invalid token" });
+  }
 }
 
 const Sem2AttendanceReport = async (req, res) => {
