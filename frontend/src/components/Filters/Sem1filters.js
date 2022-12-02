0d992b8cfc13c5f58d04ba4b73f84ab2655d2636
abhishekty17 <abhishekty17@gmail.com>
2022-12-02 13:06:18 +0530

    commit

--- frontend/src/components/Filters/Sem1filters.js
+++ frontend/src/components/Filters/Sem1filters.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { useState,navigate } from 'react';
+import { useState,navigate,useEffect,Navbar,useRef, useReactToPrint } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import jsPDF from "jspdf";
+import autoTable from 'jspdf-autotable';
+import './att.css'
+import List from './List';
 import { Link } from 'react-router-dom';
-const Sem1filters = ({subjectval,setsubjectval,dateval,setdateval,monthval,setmonthval}) => {
+var XLSX = require("xlsx");
+const Sem1filters = () => {
     const [subject,setsubject]=useState(false)
     const [date,setdate]=useState(false)
     const [month,setmonth]=useState(false)
     const[button,setbutton]=useState(false)
     const[filter,setfilter]=useState("")
-  
+    const [report,setReport]=useState(false)
+    const [subjectval, setsubjectval] = useState("")
+  const [dateval, setdateval] = useState("")
+  const [monthval, setmonthval] = useState("")
+  const[val,setval]=useState("")
+  const[student,setstudent]=useState([])
+
     function handlechange(e){
         var val=e.target.value
         setfilter(val)
@@ -50,7 +61,66 @@ const Sem1filters = ({subjectval,setsubjectval,dateval,setdateval,monthval,setmo
             setdateval("")
             setmonthval("")
         }
+        
     }
+    setReport(true)
+      const fetchdata = async () => {
+        const response = await fetch("http://localhost:4000/attendancereport/sem1", {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          }
+        })
+        const json = await response.json()
+        if (dateval) {
+          let data1 = json.data.filter((data) => data.date == dateval)
+          setstudent(data1)
+          setval(dateval)
+        }
+        else if (monthval) {
+          let data1 = json.data.filter((data) => data.date.slice(5, 7) == monthval)
+          setstudent(data1)
+          const date = new Date();
+          date.setMonth(monthval - 1);
+    
+          var month = date.toLocaleString('en-US', {
+            month: 'long',
+          });
+          setval(month)
+        }
+        else if (subjectval) {
+          let data1 = json.data.filter((data) => data.subject == subjectval)
+          setstudent(data1)
+          setval(subjectval)
+        }
+        else {
+          setstudent(json.data)
+        }
+    
+      }
+      useEffect(() => {
+    
+        fetchdata()
+    
+      }, [])
+      
+    
+      // const exporttoexcelhandler = () => {
+      //   var wb = XLSX.utils.book_new(),
+      //     ws = XLSX.utils.json_to_sheet(student);
+      //   XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
+      //   XLSX.writeFile(wb, "MyExcel.xlsx")
+      // };
+    
+      // const exporttopdfhandler = () => {
+      //   const doc = new jsPDF()
+      //   let heading = val ? "Attendance Report of " + val : "Overall Attendance Report of Semester1";
+      //   doc.text(heading, 20, 10)
+      //   autoTable(doc, { html: '#mytable' })
+      //   doc.save('table.pdf')
+      // };
+    
   return (
     <>
      <div className=" mb-3">
@@ -144,8 +214,31 @@ const Sem1filters = ({subjectval,setsubjectval,dateval,setdateval,monthval,setmo
 </form>}
    
     
-{button &&   <Link to="/attendancereport/sem1"><button type="submit" className="btn btn-primary"  >Print Attendance</button></Link> }
+{button && <button type="submit" className="btn btn-primary"  >Print Attendance</button>}
+{report && <div> <Navbar />
+      <div className='attrep-1'>
+        {val ? <h1 className='attrep-1a'> Attendance Report of {val} </h1> : <h1 className='attrep-1a'>Overall Attendance Report of Semester-1</h1>}
+      </div>
 
+      <div className='table-2'>
+        <table className='table table-striped' id='mytable'>
+          <thead className='heading-2'>
+            <tr>
+              <th>Student</th>
+              <th>Attendance Status</th>
+              <th>Date</th>
+              <th>subject</th>
+            </tr>
+          </thead>
+          <tbody>
+            <List student={student} />
+          </tbody>
+        </table>
+      </div>
+      <div className='text-center'>
+        <button type="button" class="btn btn-primary" id='butn' data-toggle="button" aria-pressed="false" autocomplete="off" >Download in excel</button>
+        <button type="button" class="btn btn-primary" id='butn' data-toggle="button" aria-pressed="false" autocomplete="off">Download in pdf</button>
+      </div></div>}
     </>
   )
 }
