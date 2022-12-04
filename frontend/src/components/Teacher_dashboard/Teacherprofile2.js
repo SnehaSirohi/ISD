6540e5d32c068875cafc75f0d247190440785296
abhishekty17 <abhishekty17@gmail.com>
2022-12-04 20:13:28 +0530

    commit

--- frontend/src/components/Teacher_dashboard/Teacherprofile2.js
+++ frontend/src/components/Teacher_dashboard/Teacherprofile2.js
@@ -4,73 +4,80 @@ import './teacherprofile2.css'
 function Teacherprofile2() {
     return (
         <>
-
-            <div class="container emp-profile">
-                <form method="post">
-                    <div class="row">
-                        <div>
-                            <div class="profile-img">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+            <h1 className='Teacherheading'>Teacher Profile</h1>
+            <div class="emp-profile">
+                <div>
+                    <form method="post">
+                        <div class="row">
+                            <div>
+                                <div class="profile-img">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+                                </div>
+                            </div>
+                            <div class="profile-head">
+                                <h4>
+                                    Kshiti Ghelani
+                                </h4>
+                                <h6>
+                                    Web Developer and Designer
+                                </h6>
                             </div>
                         </div>
-                        <div class="profile-head">
-                            <h5>
-                                Kshiti Ghelani
-                            </h5>
-                            <h6>
-                                Web Developer and Designer
-                            </h6>
-                        </div>
-                    </div>
-                    <div class="row teacherInfo">
-                        <div class="col-md-8">
-                            <div class="tab-content profile-tab" id="myTabContent">
-                                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <label>User Id</label>
+                        <div class="row teacherInfo">
+                            <div class="col-md-8">
+                                <div class="tab-content profile-tab" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>User Id</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Kshiti123</p>
+                                            </div>
                                         </div>
-                                        <div class="col-md-6">
-                                            <p>Kshiti123</p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Name</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Kshiti Ghelani</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <label>Name</label>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>kshitighelani@gmail.com</p>
+                                            </div>
                                         </div>
-                                        <div class="col-md-6">
-                                            <p>Kshiti Ghelani</p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Phone</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>123 456 7890</p>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <label>Email</label>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>kshitighelani@gmail.com</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <label>Phone</label>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>123 456 7890</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <label>Profession</label>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <p>Web Developer and Designer</p>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Profession</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Web Developer and Designer</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                        <div className='profilebuttons'>
+                            <button>Logout</button>
+                            <button>Reset Password</button>
+                        </div>
+                    </form>
+                </div>
+
             </div>
         </>
     )
