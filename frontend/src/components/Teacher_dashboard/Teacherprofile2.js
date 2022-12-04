70355b573d28d3c9b6d4a5a51a87ac97304de0d3
abhishekty17 <abhishekty17@gmail.com>
2022-12-04 19:19:54 +0530

    commiiit

--- frontend/src/components/Teacher_dashboard/Teacherprofile2.js
+++ frontend/src/components/Teacher_dashboard/Teacherprofile2.js
@@ -3,51 +3,67 @@ import './teacherprofile2.css'
 
 function Teacherprofile2() {
     return (
-        <div>
+        <>
+
             <div class="container emp-profile">
                 <form method="post">
-                    <div>
-                        <div class="col-md-4">
+                    <div class="row">
+                        <div>
                             <div class="profile-img">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
-                                <div class="file btn btn-lg btn-primary">
-                                    Change Photo
-                                    <input type="file" name="file" />
-                                </div>
                             </div>
                         </div>
-                        <div>
-                            <div class="profile-head">
-                                <h5>
-                                    Kshiti Ghelani
-                                </h5>
-                                <h6>
-                                    Web Developer and Designer
-                                </h6>
-                                <h4>About</h4>
-                                <div>
-                                    <div class="tab-content profile-tab" id="myTabContent">
-                                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                            <div>
-                                                <label>User Id</label>
-                                                <p>Kshiti123</p>
-                                            </div>
-                                            <div>
-                                                <label>Name</label>
-                                                <p>Kshiti Ghelani</p>
-                                            </div>
-                                            <div>
-                                                <label>Email</label>
-                                                <p>kshitighelani@gmail.com</p>
-                                            </div>
-                                            <div>
-                                                <label>Phone</label>
-                                                <p>123 456 7890</p>
-                                            </div>
-                                            <div >
-                                                <label>Profession</label>
-                                                <p>Web Developer and Designer</p>
-                                            </div>
+                        <div class="profile-head">
+                            <h5>
+                                Kshiti Ghelani
+                            </h5>
+                            <h6>
+                                Web Developer and Designer
+                            </h6>
+                        </div>
+                    </div>
+                    <div class="row teacherInfo">
+                        <div class="col-md-8">
+                            <div class="tab-content profile-tab" id="myTabContent">
+                                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label>User Id</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>Kshiti123</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label>Name</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>Kshiti Ghelani</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label>Email</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>kshitighelani@gmail.com</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label>Phone</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>123 456 7890</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label>Profession</label>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p>Web Developer and Designer</p>
                                         </div>
                                     </div>
                                 </div>
@@ -56,7 +72,7 @@ function Teacherprofile2() {
                     </div>
                 </form>
             </div>
-        </div>
+        </>
     )
 }
 
