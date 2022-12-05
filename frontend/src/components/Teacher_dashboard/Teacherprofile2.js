ad5f899370d4f903ace452807bc65209c65756b7
Dipanshuchandran7 <dipanshu.2021@iic.ac.in>
2022-12-05 20:56:20 +0530

    profile

--- frontend/src/components/Teacher_dashboard/Teacherprofile2.js
+++ frontend/src/components/Teacher_dashboard/Teacherprofile2.js
@@ -1,26 +1,27 @@
 import React from 'react'
+import Navbar from './Navbar'
 import './teacherprofile2.css'
+// import Navbar from './Navbar'
 
 function Teacherprofile2() {
     return (
         <>
+        <Navbar/>
             <h1 className='Teacherheading'>Teacher Profile</h1>
             <div class="emp-profile">
                 <div>
                     <form method="post">
-                        <div class="row">
-                            <div>
+                        <div className='photo_block'>
                                 <div class="profile-img">
                                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
                                 </div>
-                            </div>
                             <div class="profile-head">
-                                <h4>
+                                <h2>
                                     Kshiti Ghelani
+                                </h2>
+                                <h4>
+                                   Asst. Lecturer
                                 </h4>
-                                <h6>
-                                    Web Developer and Designer
-                                </h6>
                             </div>
                         </div>
                         <div class="teacherInfo">
@@ -29,7 +30,7 @@ function Teacherprofile2() {
                                     <label>User Id: </label>
                                 </div>
                                 <div>
-                                    <label>Name: </label>
+                                    <label>DOB </label>
                                 </div>
                                 <div>
                                     <label>Email: </label>
@@ -40,25 +41,25 @@ function Teacherprofile2() {
                             </div>
                             <div className='values'>
                                 <div>
-                                    <p>Kshiti Ghelani</p>
+                                    <p>Kshiti_Ghelani</p>
+                                </div>
+                                <div>
+                                    <p>28/11/2000</p>
                                 </div>
                                 <div>
                                     <p>kshitighelani@gmail.com</p>
                                 </div>
+
                                 <div>
                                     <p>123 456 7890</p>
                                 </div>
 
-                                <div>
-                                    <p>Web Developer and Designer</p>
-                                </div>
-
                             </div>
                         </div>
-                        <div className='profilebuttons'>
-                            <button>Logout</button>
-                            <button>Reset Password</button>
-                        </div>
+                        <div className='text-center pt-4'>
+                                 <button id='butn' class="btn btn-primary" >Logout</button>
+                                 <button id='butn' class="btn btn-primary-1" >Reset Password</button>
+                         </div>
                     </form>
                 </div>
 
