7f5683343941b56836b279dcbc80ff8d71b3400f
abhishekty17 <abhishekty17@gmail.com>
2022-12-09 11:06:07 +0530

    commit

--- frontend/src/components/Teacher_dashboard/Teacherprofile2.js
+++ frontend/src/components/Teacher_dashboard/Teacherprofile2.js
@@ -26,37 +26,28 @@ function Teacherprofile2() {
                         </div>
                         <div class="teacherInfo">
                             <div className='keys'>
-                                <div>
-                                    <label>User Id: </label>
-                                </div>
-                                <div>
-                                    <label>DOB </label>
-                                </div>
-                                <div>
-                                    <label>Email: </label>
-                                </div>
-                                <div>
-                                    <label>Phone: </label>
-                                </div>
+                                <p>User Id: </p>
+                                <p>DOB </p>
+                                <p>Email: </p>
+                                <p>Phone: </p>
                             </div>
                             <div className='values'>
-                                <div>
-                                    <p>Kshiti_Ghelani</p>
-                                </div>
-                                <div>
-                                    <p>28/11/2000</p>
-                                </div>
-                                <div>
-                                    <p>kshitighelani@gmail.com</p>
-                                </div>
-
-                                <div>
-                                    <p>123 456 7890</p>
-                                </div>
-
+                                <p>Kshiti_Ghelani</p>
+                                <p>28/11/2000</p>
+                                <p>kshitighelani@gmail.com</p>
+                                <p>123 456 7890</p>
+                            </div>
+                            <div className='teacherinfo2'>
+                                <p>User Id: </p>
+                                <h6>Kshiti_Ghelani</h6>
+                                <p>DOB </p>
+                                <h6>28/11/2000</h6>
+                                <p>Email: </p>
+                                <h6>kshitighelani@gmail.com</h6>
+                                <p>Phone: </p>
+                                <h6>123 456 7890</h6>
                             </div>
                         </div>
-
                     </form>
                 </div>
                 <div className='text-center' id='prof_block'>
