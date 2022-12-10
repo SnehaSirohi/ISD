f7c79aea7772255b25f001d0b9717d4c6cd53ca1
AnasSaifeee <anas.saifi@iic.ac.in>
2022-12-10 14:49:35 +0530

    cmmt

--- frontend/src/components/Assignment_Upload/Upload_assignment_sem4.js
+++ frontend/src/components/Assignment_Upload/Upload_assignment_sem4.js
@@ -16,7 +16,7 @@ function UploadAssignmentsem4() {
   const [NitishaAgg, setNitishaAgg] = useState(false);
   const [MKDas, setMKDas] = useState(false);
   const [Sanjeev, setSanjeev] = useState(false);
-
+  const [success,setsuccess]=useState(false)
   async function populate(e) {
     const req = await fetch("http://localhost:4000/upload/assignment", {
       headers: {
@@ -68,6 +68,10 @@ function UploadAssignmentsem4() {
       });
   
       const data = await response.json();
+       setsuccess(data.success)
+      setTimeout(() => {
+        setsuccess(false)
+      }, 2500);
     }
    
   }
@@ -87,6 +91,7 @@ function UploadAssignmentsem4() {
     }
   }, []);
   return (
+    <>
     <div className="uploadassignmentbody">
       <Navbar />
       <div className="uploadheading">
@@ -216,6 +221,15 @@ function UploadAssignmentsem4() {
         </div>
       </div>
     </div>
+    {success &&  <div className="container-fluid blacky">
+    <div className="success">
+   <div classNam="wrappertick"> <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"> <circle className="checkmark__circle" cx={26} cy={26} r={25} fill="none"/> <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+</svg>
+</div>
+<h4>Assignment Posted</h4>
+</div>
+      </div>}
+      </>
   );
 }
 
--- frontend/src/components/Teacher_dashboard/Teacher_profile.js
+++ frontend/src/components/Teacher_dashboard/Teacher_profile.js
@@ -49,42 +49,69 @@ const Teacher_Profile = () => {
         }
     }, [name], [email])
 
-    return (
+     return (
         <>
-            <div >
-                <Navbar />
-                <div className='profilebody'>
-                    <div className='profileContent'>
-                        <div className='profileenroll'>
-                            {/* <img src={logo} alt="" class="avatar mt-1"></img> */}
-                            <div class="mt-3">
-                                <h3>{name}</h3>
-                                <h3>Professor ID: {teacher_id || 'ish'}</h3>
-                                <img src={logo} alt="..." class="profilepicture"></img>
+            <Navbar />
+            <h1 className='Teacherheading'>Teacher Profile</h1>
+            <div class="emp-profile">
+                <div className='pblock'>
+                    <form method="post">
+                        <div className='photo_block'>
+                            <div class="profile-img">
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+                            </div>
+                            <div class="profile-head">
+                                <h2>
+                                   {name}
+                                </h2>
+                                <h4>
+                                    {teacher_id}
+                                </h4>
                             </div>
                         </div>
-                        <div class="mb-2 detailblock">
-                            <div class="p-0 mb-2 detailcontent">
-                                <div class="col d-flex flex-column position-static details">
-                                    <h3 class="mb-3 ">Personal Details</h3>
-                                    <h6>Contact no. - {contactNum}</h6>
-                                    <h6>Mobile no - xxxxxxxxxx </h6>
-                                    <h6>D.O.B - 17/11/2000 </h6>
-                                    <h6>Mail ID - {email} </h6>
-                                </div>
+                        <div class="teacherInfo">
+                            <div className='keys'>
+                                <p>User Id: </p>
+                                <p>DOB </p>
+                                <p>Email: </p>
+                                <p>Phone: </p>
+                            </div>
+                            <div className='values'>
+                                <p> {teacher_id}</p>
+                                <p>28/11/2000</p>
+                                <p>{email}</p>
+                                <p>123 456 7890</p>
+                            </div>
+                            <div className='teacherinfo2'>
+                                <p>User Id: </p>
+                                <h6>{teacher_id}</h6>
+                                <p>DOB </p>
+                                <h6>28/11/2000</h6>
+                                <p>Email: </p>
+                                <h6>{email}</h6>
+                                <p>Phone: </p>
+                                <h6>{contactNum}</h6>
                             </div>
                         </div>
-                        <div className='buttons'>
+                    </form>
+                </div>
+                 <div className='text-center' id='prof_block'>
+                    <button id='butn' class="btn btn-primary"  onClick={() => {
+                                localStorage.removeItem('token')
+                                navigate("/")
+                            }}>Logout</button>
+                    <button id='butn' class="btn btn-primary-1" >Reset Password</button>
+                </div>
+    </div>
+                        {/* <div className='buttons'>
                             <button onClick={() => {
                                 localStorage.removeItem('token')
                                 navigate("/")
                             }}>Logout</button>
                             <button className="resetpass"><Link to="/Teacherdashboard/changepassword"><div>Reset Password</div></Link></button>
-                        </div>
+                        </div> */}
 
-                    </div>
-                </div>
-            </div>
+                 
 
         </>
     );
