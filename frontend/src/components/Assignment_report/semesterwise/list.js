700c34f45d2a17aaff4fa3c7670fc48809dce818
AnasSaifeee <anas.saifi@iic.ac.in>
2022-12-17 23:51:53 +0530

    cmmt

--- frontend/src/components/Assignment_report/semesterwise/list.js
+++ frontend/src/components/Assignment_report/semesterwise/list.js
@@ -43,7 +43,7 @@ const List = ({ assignments, AssignmentSubmit, files, setfile, key, removefileid
                       )
                    setassignment_id(_id)
                      }} />
-                  <button className='upload_button'>Upload</button>
+                  <button className='upload_button' style={{backgroundColor:'#007bff', color:'white'}}>Upload</button>
                      {/* <span>{files}</span> */}
                   <span>{removefileid !== _id && (localStorage.getItem(_id) || (_id===temp.id && temp.name)) }</span>
                   <button className='submit_button' type='submit' onClick={   (e)=>{AssignmentSubmit(e, _id)}}>Submit</button>
